6d888d0a4d627ec7af87ce59076c1aea148dd9b8
winter-py <isosteven@gmail.com>
2022-04-02 07:12:59 -0400

    Project Idea to start one

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+a = input('insert a number')
+b = input('insert a number')
+c = input('insert a number')
+
+
+def add(a):
+    result = a + b
+    return result
+
+def sub(a):
+    result = a - b
+    return result
+
+def divide(a):
+    result = a / b
+    return result
+
+def muilt(a):
+    result = a * b
+    return result
+
+print (muilt(9))
