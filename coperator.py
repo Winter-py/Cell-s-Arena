705649ffdbda77b3a687cc771346bb32e40f8b4f
Winter-py <isosteven@gmail>
2022-04-04 21:51:24 +0100

    operators added

---
+++ coperator.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    result = a + b
+    return result
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+def divide(a, b):
+    result = a / b
+    return result
