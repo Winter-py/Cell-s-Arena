4252e32ffd33be3b3a86e47f355c78dee71d1aa1
Winter-py <isosteven@gmail>
2022-04-04 21:50:13 +0100

    All operators functioning

--- calculator.py
+++ calculator.py
@@ -1,10 +1,36 @@
-
-a = float (input('insert a number'))
-b = float (input('insert a number'))
-
+a = float (input('insert a number '))
+operator = input('insert an operator ')
+b = float (input('insert a number '))
 
 def add(a, b):
     result = a + b
     return result
 
-print(add(a, b))
+def subtract(a, b):
+    result = a - b
+    return result
+
+def multiply(a, b):
+    result = a * b
+    return result
+
+def divide(a, b):
+    result = a / b
+    return result
+
+# Runa all the calculations through a if statement
+if operator == '+':
+    print(add(a, b))
+elif operator == '-':
+    print(subtract(a, b))
+elif operator == '*':
+    print(multiply(a, b))
+elif operator == '/':
+    print(divide(a, b))
+elif operator == '**':
+    print(a ** b)
+elif operator == '//':
+    print(a // b)   
+else:
+    print('invalid operator')
+
