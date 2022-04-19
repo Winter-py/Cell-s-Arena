6273ca579185121ed7711bdfddd8fe5a6fabb81e
Winter-py <isosteven@gmail>
2022-04-19 15:37:48 +0100

    Rock statment done

--- Rock,paper,scissors.py
+++ Rock,paper,scissors.py
@@ -1,1 +1,16 @@
 #Rock, paper, scissors
+import random
+
+hands = ["Rock", "Paper", "Scissors"]
+
+user_input = input("Rock, paper, scissors? ")
+
+random.hand = random.choice(hands)
+
+if user_input == random.hand:
+    print("It's a tie!")
+elif user_input == "Rock":
+    if random.hand == "Paper":
+        print("You lose!", random.hand, "covers", user_input)
+    else:
+        print("You win!", user_input, "smashes", random.hand)
